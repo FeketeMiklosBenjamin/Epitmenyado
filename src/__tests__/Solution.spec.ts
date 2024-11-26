c013a246bb63218ca080ca89b7236e8747aa0de2
FeketeMiklosBenjamin <fekete.miklos.benjamin@students.jedlik.eu>
2024-11-26 16:56:08 +0100

    2. feladat ellenőrzése

--- src/__tests__/Solution.spec.ts
+++ src/__tests__/Solution.spec.ts
@@ -1,7 +1,9 @@
-
 import Solution from "../Solution";
 
 describe("Solution osztály tesztek", () => {
     const instance = new Solution("utca.txt");
     
+    it("2. feladat ellenőrzése", async () => {
+        expect(instance.streetTaxesCount).toBe(543);
+    });
 })
