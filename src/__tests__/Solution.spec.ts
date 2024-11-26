ac5ffa44f469a79d04a0f4d46e5e5b74c47f7b8c
Domi_Nate <jdomi@gmail.com>
2024-11-26 17:07:21 +0100

    3. feladat ellenőrzése

--- src/__tests__/Solution.spec.ts
+++ src/__tests__/Solution.spec.ts
@@ -3,7 +3,11 @@ import Solution from "../Solution";
 describe("Solution osztály tesztek", () => {
     const instance = new Solution("utca.txt");
     
-    it("2. feladat ellenőrzése", async () => {
+    it("2. feladat ellenőrzése", () => {
         expect(instance.streetTaxesCount).toBe(543);
+    });
+    it("3. feladat ellenőrzése", () => {
+        expect(instance.getOwnerHouses("68396").has("Harmat")).toBe(true);
+        expect(instance.getOwnerHouses("68396").has("Szepesi")).toBe(true);
     });
 })
