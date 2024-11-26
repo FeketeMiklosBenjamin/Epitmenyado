40ff074c2119b56f70d8cc951952a0b27b87f28e
Domi_Nate <jdomi@gmail.com>
2024-11-26 17:20:16 +0100

    6. feladat ellenőrzése

--- src/__tests__/Solution.spec.ts
+++ src/__tests__/Solution.spec.ts
@@ -15,4 +15,12 @@ describe("Solution osztály tesztek", () => {
         expect(instance.taxCalculatorString("B").trim()).toBe("B sávba 144 telek esik, az adó 13107000 Ft.");
         expect(instance.taxCalculatorString("C").trim()).toBe("C sávba 234 telek esik, az adó 3479600 Ft.");
     });
+    it("6. feladat ellenőrzése", () => {
+        expect(instance.moreDistanceTaxedStreet()).toContain("Besztercei");
+        expect(instance.moreDistanceTaxedStreet()).toContain("Gyurgyalag");
+        expect(instance.moreDistanceTaxedStreet()).toContain("Icce");
+        expect(instance.moreDistanceTaxedStreet()).toContain("Kurta");
+        expect(instance.moreDistanceTaxedStreet()).toContain("Rezeda");
+        expect(instance.moreDistanceTaxedStreet()).toContain("Szepesi");
+    });
 })
