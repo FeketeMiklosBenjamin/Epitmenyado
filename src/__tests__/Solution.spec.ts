88e45b0094763f894f260745365a35e137a9237e
FeketeMiklosBenjamin <fekete.miklos.benjamin@students.jedlik.eu>
2024-11-26 17:15:40 +0100

    5. feladat ellenőrzése

--- src/__tests__/Solution.spec.ts
+++ src/__tests__/Solution.spec.ts
@@ -10,4 +10,9 @@ describe("Solution osztály tesztek", () => {
         expect(instance.getOwnerHouses("68396").has("Harmat")).toBe(true);
         expect(instance.getOwnerHouses("68396").has("Szepesi")).toBe(true);
     });
+    it("5. feladat ellenőrzése", () => {
+        expect(instance.taxCalculatorString("A").trim()).toBe("A sávba 165 telek esik, az adó 20805600 Ft.");
+        expect(instance.taxCalculatorString("B").trim()).toBe("B sávba 144 telek esik, az adó 13107000 Ft.");
+        expect(instance.taxCalculatorString("C").trim()).toBe("C sávba 234 telek esik, az adó 3479600 Ft.");
+    });
 })
